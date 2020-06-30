6d79d245c4be05be89bbbe5d32620cb81d9c6bc2
Tuan Luong <tuanluong.it@gmail.com>
2020-06-30 22:26:30 +0700

    add personas package

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {PersonasAvatar} from 'react-native-personas-avatar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <PersonasAvatar style={{ width: 200, height: 200 }} />
     </View>
   );
 }
