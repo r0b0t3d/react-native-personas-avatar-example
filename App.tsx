d9fad7009bb15d6de3917a733859235e7479cc07
Tuan Luong <tuanluong.it@gmail.com>
2020-07-01 22:53:18 +0700

    add example for randomize

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {PersonasAvatar} from 'react-native-personas-avatar';
+import { StatusBar } from "expo-status-bar";
+import React, { useRef } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { PersonasAvatar } from "react-native-personas-avatar";
 
 export default function App() {
+  const avatar = useRef<any>();
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <PersonasAvatar style={{ width: 200, height: 200 }} />
+      <PersonasAvatar ref={avatar} style={{ width: 200, height: 200 }} />
+      <TouchableOpacity
+        style={styles.randomizeButton}
+        onPress={() => avatar.current.randomize()}
+      >
+        <Text style={{ color: "white", fontSize: 16 }}>Randomize</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,8 +22,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  randomizeButton: {
+    marginTop: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    backgroundColor: "gray",
+    borderRadius: 5,
   },
 });
